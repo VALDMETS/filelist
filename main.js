183f2967bb0660c7eb233e816312d402bc2afe63
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-06-29 16:32:11 -0500

    done with it

--- main.js
+++ main.js
@@ -7,14 +7,12 @@ var fullInfo =
   {title: 'Dropbox', amt: 1},
   {title: 'Skydrive', amt: 10},
 ] ,
-
 mail: [
   {title: 'Work', amt: 12},
   {title: 'Mom', amt: 3},
   {title: 'The Band', amt: 7},
   {title: 'Wife', amt: 4},
 ] ,
-
 cloud: [
   {title: 'Connect', amt: 15},
   {title: 'Profiles', amt: 23},
@@ -23,22 +21,26 @@ cloud: [
   {title: 'Videos', amt: 4},
   {title: 'Documents', amt: 95},
 ] };
-console.log(fullInfo);
+
 var alreadyOpen = {files: 0, mail: 0, cloud: 0};
 var $sectionSelect = $('.filehead');
 var $ol = ('<ol class="openMenu"></ol>');
-// console.log($sectionSelect);
+
+document.querySelector('#filesCounter').innerText = fullInfo.files.reduce(getAmt,0);
+document.querySelector('#mailCounter').innerText = fullInfo.mail.reduce(getAmt,0);
+document.querySelector('#cloudCounter').innerText = fullInfo.cloud.reduce(getAmt,0);
+
+function getAmt (returnVal, current, i, arr) {
+  current = current['amt'];
+  return returnVal + current;
+}
 
 $sectionSelect.click(function(e){
   $sectionSelect.removeClass('active');
   $(this).addClass('active');
   var titler = String(e.target.id);
   var menuSpot = '#' + titler + 'Menu';
-  console.log(menuSpot);
   if (alreadyOpen[titler] === 0) {
-    // $(this).append($ol);
-    console.log(titler);
-    console.log(fullInfo[titler]);
       for (var i = 0; i < fullInfo[titler].length; i++) {
         var liName = fullInfo[titler][i].title;
         var liAmt = fullInfo[titler][i].amt;
